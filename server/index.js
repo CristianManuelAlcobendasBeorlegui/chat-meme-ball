3a2480b8f9c8f77d8d70d7f7a85e47f4d99feed5
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-05-11 17:25:29 +0200

    Se añade persistencia a los mensajes que se envian

--- server/index.js
+++ server/index.js
@@ -6,15 +6,25 @@ import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import sqlite3 from 'sqlite3';
 
 // === CONSTANTES === //
 const port = 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const db = new sqlite3.Database("database.sqlite");
 
 // === LOGGER === //
 app.use(logger('dev'));
+
+// === CONFIGURACION DE LA BASE DE DATOS === //
+db.exec(
+    "CREATE TABLE IF NOT EXISTS messages ("
+    +   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    +   "content TEXT"
+    + ")"
+);
 
 // === CONFIGURACION DEL SERVIDOR === //
 io.on("connection", function(socket) {
@@ -24,11 +34,14 @@ io.on("connection", function(socket) {
         console.log("Se ha desconectado un usuario.");
     });
 
-    socket.on("chat message", function(message) {
+    socket.on("chat message", async function(message) {
+        // Guarda el mensaje en BD
+        db.exec("INSERT INTO messages (content) VALUES ('" + message + "')");
+        
         // Envia el mensaje a todos los usuarios conectados (Broadcast)
         io.emit("chat message", message);
     });
-})
+});
 
 server.listen(port, function() {
     console.log("Server is listening to *:" + port);
